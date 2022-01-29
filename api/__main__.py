eecd960a98b359d7329bc9ae524656a8a84cc673
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2022-01-29 17:28:53 -0300

    feat: add create_all at application start

--- api/__main__.py
+++ api/__main__.py
@@ -1,9 +1,11 @@
 import uvicorn
 
 from api.app import create_app
+from api.database.loader import create_all
 from api.settings import deploy_settings
 
 app = create_app()
+create_all()
 
 if __name__ == "__main__":
     uvicorn.run(
