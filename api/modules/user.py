e1fe807fa176437b65a3b6f05160c4cc581e0ac7
Matheus Sena Vasconcelos <sena.matheus14@gmail.com>
2022-01-29 17:42:50 -0300

    feat: create user module

--- api/modules/user.py
+++ api/modules/user.py
@@ -2,4 +2,4 @@ import re
 
 
 def is_valid_email(email: str) -> bool:
-    return re.match('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$', email) is not None
+    return re.match(r"^[a-zA-Z0-9]+[._]?[a-zA-Z0-9]+[@]\w+[.]\w{2,3}$", email) is not None
